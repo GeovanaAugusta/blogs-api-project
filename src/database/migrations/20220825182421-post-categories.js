dd3421d2e2bbd3c279e5fcb54ac1ecde9c5e4f8d
Geovana Augusta <geovanaag@hotmail.com>
2022-08-25 16:21:12 -0300

    feat: 01 - generate  migrations and creating tables

---
+++ src/database/migrations/20220825182421-post-categories.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const PostCategoriesTable = await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      }
+    });
+    return PostCategoriesTable;
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
