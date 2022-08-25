e41d99883ae8c977481b840250f1666e8147a957
Geovana Augusta <geovanaag@hotmail.com>
2022-08-25 18:29:39 -0300

    fix: complete ids in models

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
-    id: DataTypes.INTEGER,
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING,
   });
 
